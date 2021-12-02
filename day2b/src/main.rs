c29583949aa942d622912169f1555da676156db5
Anuvrat <anuvrats06@gmail.com>
2021-12-02 16:37:28 +0530

    done with day2b

--- day2b/src/main.rs
+++ day2b/src/main.rs
@@ -6,23 +6,32 @@ fn main() -> Result<()> {
     let file = File::open(Path::new("input.txt"))?;
     let reader = BufReader::new(file);
 
-    let mut horizontal : usize = 0;
-    let mut depth : usize= 0;
+    let mut horizontal: usize = 0;
+    let mut depth: usize = 0;
+    let mut aim: usize = 0;
 
+    // Can be done with regex but I don't know
     for line in reader.lines() {
         let newline = line?.parse::<String>().unwrap();
         let array: Vec<&str> = newline.split(" ").collect();
         let value = array[1].parse::<usize>().unwrap();
 
         match array[0] {
-            "forward" => horizontal = horizontal + value,
-            "up" => depth = depth - value,
-            "down" => depth = depth + value,
+            "forward" => {
+                if aim != 0 {
+                    depth = depth + (value * aim);
+                    horizontal = horizontal + value;
+                } else {
+                    horizontal = horizontal + value
+                }
+            }
+            "up" => aim = aim - value,
+            "down" => aim = aim + value,
             _ => panic!("Value not supported"),
         };
-        // println!("h:{} d:{}", horizontal, depth);
     }
-    println!("{}", horizontal * depth );
+    // println!("h:{} d:{}, aim:{}", horizontal, depth, aim);
+    println!("Result: {}", horizontal * depth);
 
     Ok(())
 }
