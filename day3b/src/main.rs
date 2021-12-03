4dad45f386f0f66559332d368fa352d3cd08cf80
Anuvrat <anuvrats06@gmail.com>
2021-12-03 22:39:59 +0530

    I know there exists a one liner for this, but I'm still proud

--- day3b/src/main.rs
+++ day3b/src/main.rs
@@ -13,8 +13,13 @@ fn main() -> Result<()> {
         input_array.push(line?.parse::<String>().unwrap());
     }
 
-    find_max_min(input_array.clone(), MaxMin::Max);
-    find_max_min(input_array.clone(), MaxMin::Min);
+    let (oxygen_generator_rating, _) = find_max_min(input_array.clone(), MaxMin::Max, None);
+    let (co2_scrubber_rating, _) = find_max_min(input_array.clone(), MaxMin::Min, None);
+
+    
+
+
+
 
     Ok(())
 }
@@ -31,7 +36,21 @@ enum MaxMin {
     Min,
 }
 
-fn find_max_min(input: Vec<String>, interest: MaxMin) -> Vec<String> {
+fn find_max_min(
+    input: Vec<String>,
+    interest: MaxMin,
+    iter_input: Option<usize>,
+) -> (Vec<String>, Option<usize>) {
+    let iter: usize;
+    match iter_input {
+        Some(_) => {
+            iter = iter_input.unwrap();
+        }
+        None => {
+            iter = 0;
+        }
+    }
+
     if input.len() > 1 {
         match interest {
             MaxMin::Max => {
@@ -39,18 +58,71 @@ fn find_max_min(input: Vec<String>, interest: MaxMin) -> Vec<String> {
                 let mut zero = 0;
                 let mut one = 0;
 
-                while one < input.len() / 2 || zero < input.len() / 2 {
-                    for i in input {
-                        for j in input[i] {}
+                for element_one in &input {
+                    if element_one.as_bytes()[iter] as char == 48 as char {
+                        zero = zero + 1;
+                    } else {
+                        one = one + 1;
                     }
                 }
-                max_output
+                if zero > one {
+                    for element_two in input {
+                        if element_two.as_bytes()[iter] as char == 48 as char {
+                            max_output.push(element_two);
+                        }
+                    }
+                } else if zero == one {
+                    for element_two in input {
+                        if element_two.as_bytes()[iter] as char == 49 as char {
+                            max_output.push(element_two)
+                        }
+                    }
+                } else {
+                    for element_two in input {
+                        if element_two.as_bytes()[iter] as char == 49 as char {
+                            max_output.push(element_two);
+                        }
+                    }
+                }
+
+                find_max_min(max_output, MaxMin::Max, Some(iter + 1))
             }
             MaxMin::Min => {
-                println!("Min {:?}", input);
+                let mut min_output: Vec<String> = Vec::new();
+                let mut zero = 0;
+                let mut one = 0;
+
+                for element_one in &input {
+                    if element_one.as_bytes()[iter] as char == 48 as char {
+                        zero = zero + 1;
+                    } else {
+                        one = one + 1;
+                    }
+                }
+                if zero < one {
+                    for element_two in input {
+                        if element_two.as_bytes()[iter] as char == 48 as char {
+                            min_output.push(element_two);
+                        }
+                    }
+                } else if zero == one {
+                    for element_two in input {
+                        if element_two.as_bytes()[iter] as char == 48 as char {
+                            min_output.push(element_two);
+                        }
+                    }
+                } else {
+                    for element_two in input {
+                        if element_two.as_bytes()[iter] as char == 49 as char {
+                            min_output.push(element_two);
+                        }
+                    }
+                }
+
+                find_max_min(min_output, MaxMin::Min, Some(iter + 1))
             }
         }
     } else {
-        input
+        (input, None)
     }
 }
