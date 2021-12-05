f13cfad5612d785810228cbfcbe38e1b7bf8eaec
Anuvrat <anuvrats06@gmail.com>
2021-12-05 16:14:03 +0530

    somehow it works but allocation to board is vertical

--- day5a/src/main.rs
+++ day5a/src/main.rs
@@ -1,17 +1,66 @@
 use std::{fs::read_to_string, path::Path};
 
 fn main() {
-    // let file = read_to_string(Path::new("test.txt")).unwrap();
-    let file = read_to_string(Path::new("input.txt")).unwrap();
+    let file = read_to_string(Path::new("test.txt")).unwrap();
+    // let file = read_to_string(Path::new("input.txt")).unwrap();
 
-    let mut _nine_by_nine: Vec<Vec<usize>> = vec![vec![0; 10]; 10];
+    let mut new_board: Vec<Vec<usize>> = vec![vec![0; 10]; 10]; //10 by 10
+                                                                // let mut new_board: Vec<Vec<usize>> = vec![vec![0; 1000]; 1000]; //1000 by 1000
 
     // println!("{:?}", nine_by_nine);
 
     let parsed_input = parse_input(&mut file.lines());
 
-    println!("Parsed Input: {:?}", parsed_input);
+    make_board(&parsed_input, &mut new_board);
+    println!("Board: {:?}", new_board);
+
+    // println!("Parsed Input: {:?}", parsed_input);
     // println!("Grid: {:?}", nine_by_nine);
+}
+
+fn make_board(input: &Vec<Vec<usize>>, board: &mut Vec<Vec<usize>>) {
+    for i in 0..input.len() {
+        let (x1, y1, x2, y2) = (input[i][0], input[i][1], input[i][2], input[i][3]); // find better way to destructre
+        if x1 == x2 {
+            if y1 > y2 {
+                for l in y2..y1 + 1 {
+                    // +1 to include y1
+                    board[x1][l] = board[x1][l] + 1;
+                    println!("Changed board y1>y2");
+                }
+                // println!("y1:{} is greater than y2:{}", y1, y2)
+            } else if y1 < y2 {
+                for l in y1..y2 + 1 {
+                    // +1 to include y2
+                    board[x1][l] = board[x1][l] + 1;
+                    println!("Changed board y1<y2");
+                }
+                // println!("y1:{} is less than y2:{}", y1, y2)
+            } else if y1 == y2 {
+                board[x1][y1] = board[x1][y1] + 1;
+                println!("Changed board y1=y2");
+                // println!("y1:{} is equal to y2:{}", y1, y2)
+            }
+            // println!("x1 is equal to x2 in iter: {}", i);
+        } else if y1 == y2 {
+            if x1 > x2 {
+                for l in x2..x1 + 1 { // +1 to include x1
+                    board[l][y1] = board[l][y1] + 1;
+                    println!("Changed board x1>x2");
+                }
+            } else if x1 < x2 {
+                for l in x1..x2 { // +1 to include x2
+                    board[l][y1] = board[l][y1] + 1;
+                    println!("Changed board x1<x2");
+                }
+            } else if x1 == x2 {
+                board[x1][y1] = board[x1][y1] + 1;
+                println!("Changed board x1=x2");
+            }
+        } else {
+            continue;
+        }
+    }
 }
 
 fn parse_input(input: &mut std::str::Lines<'_>) -> Vec<Vec<usize>> {
