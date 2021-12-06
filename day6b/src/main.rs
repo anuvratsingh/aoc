d9273f10f21015383a8ccdbdf8f72b96655cd0af
Anuvrat <anuvrats06@gmail.com>
2021-12-06 22:33:06 +0530

    Vec works

--- day6b/src/main.rs
+++ day6b/src/main.rs
@@ -1,32 +1,61 @@
 use std::{fs::read_to_string, path::Path};
 fn main() {
-    let file = read_to_string(Path::new("test.txt")).unwrap();
-    // let file = read_to_string(Path::new("input.txt")).unwrap();
+    // let file = read_to_string(Path::new("test.txt")).unwrap();
+    let file = read_to_string(Path::new("input.txt")).unwrap();
 
-    let cycle = 2;
+    let cycle = 256;
 
     let parsed_input = new_new_parser(&mut file.lines());
 
-    let _result = new_new_reproducer(parsed_input, cycle);
+    let from_reproducer = new_new_reproducer(parsed_input, cycle);
+    let result = calculate_no_of_fish(&from_reproducer);
 
     // println!("{:?}", parsed_input);
-    // println!("Parsed Input: {:?}", result);
+    // println!(
+    //     "Resulted array after {} days : {:?}",
+    //     cycle, from_reproducer
+    // );
+    println!("Result: {}", result)
+
 }
 
 // [[0, 0], [1, 1], [2, 1], [3, 2], [4, 1], [5, 0], [6, 0], [7, 0], [8, 0]]
 
+fn calculate_no_of_fish(input: &Vec<Vec<usize>>) -> usize {
+    let mut result = 0;
+    for i in 0..input.len() {
+        result = result + input[i][1];
+    }
+    result
+}
 fn new_new_reproducer(input: Vec<Vec<usize>>, cycles: usize) -> Vec<Vec<usize>> {
     let mut output = input.clone();
 
     for _ in 0..cycles {
-        for i in &mut output {
-            if i[1] > 0 {
-                i
-            } 
+        let temp_zero_value = output[0][1];
+        // println!("Output BEFORE iter {}:{:?}", l, output);
+        if output[0][1] > 0 {
+            // println!("Before Inside i=0,Iter{}, output is{:?}", l, output);
+            output[0][1] = output[0][1] - output[0][1];
         }
+        for i in 0..output.len() {
+            if i != 0 {
+                if output[i][1] != 0 {
+                    output[i - 1][1] = output[i - 1][1] + output[i][1];
+                    output[i][1] = output[i][1] - output[i][1];
+                }
+            }
+        }
+        if temp_zero_value > 0 {
+            output[6][1] = output[6][1] + temp_zero_value;
+            output[8][1] = output[8][1] + temp_zero_value;
+        }
+
+        // println!("Output AFTER day {}:{:?}", l+1, output);
     }
     output
 }
+
 fn new_new_parser(input: &mut std::str::Lines<'_>) -> Vec<Vec<usize>> {
     let array: Vec<usize> = input
         .next()
@@ -160,33 +189,5 @@ fn new_new_parser(input: &mut std::str::Lines<'_>) -> Vec<Vec<usize>> {
 //     //     }
 //     // }
 
-//     output
-// }
-
-
-// fn new_new_reproducer(input: Vec<Vec<usize>>, cycles: usize) -> Vec<Vec<usize>> {
-//     let mut output = input.clone();
-
-//     for l in 0..cycles {
-//         // println!("Output BEFORE iter {}:{:?}", l, output);
-//         for i in 0..output.len() {
-//             if i != 0 {
-//                 if output[i][1] != 0 {
-//                     output[i - 1][1] = output[i - 1][1] + output[i][1];
-//                     output[i][1] = output[i][1] - output[i][1];
-//                 }
-//             } else {
-//                 if output[0][1] > 0 {
-//                     println!("Before Inside i=0,Iter{}, output is{:?}", l, output);
-//                     output[6][1] = output[6][1] + output[0][1];
-//                     output[8][1] = output[8][1] + output[0][1];
-//                     output[0][1] = output[0][1] - output[0][1];
-//                     println!("After Inside i=0,Iter{}, output is{:?}", l, output);
-//                     break;
-//                 }
-//             }
-//         }
-//         println!("Output AFTER iter {}:{:?}", l, output);
-//     }
 //     output
 // }
