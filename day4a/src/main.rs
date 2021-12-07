2a1d88e6ca877ae5092224fef475d6f586b0e104
Anuvrat <anuvrats06@gmail.com>
2021-12-08 01:19:16 +0530

    done with day4a

--- day4a/src/main.rs
+++ day4a/src/main.rs
@@ -1,198 +1,131 @@
-use std::{fs::read_to_string, io::Result, path::Path};
+#[allow(dead_code, unused_assignments, unused_mut)]
+use std::{fs::read_to_string, path::Path};
 
-fn main() -> Result<()> {
-    let file = read_to_string(Path::new("test.txt")).unwrap(); // can't find a way to get txt as string blob from bufreader
-                                                               // println!("{:?}", file.lines().next().unwrap());
-                                                               // println!("{:?}", file.lines()[0]);
-    let (bingo_input, bingo_boards) = make_bingo_board_and_input(&mut file.lines());
+fn main() {
+    // let file = read_to_string(Path::new("test.txt")).unwrap();
+    let file = read_to_string(Path::new("input.txt")).unwrap();
 
-    // println!("Input: {:?}", bingo_boards);
+    let (bingo_input, mut bingo_board_s) = parse_input(&mut file.lines());
 
-    // for num_to_mark in bingo_input {
-    //     for single_board in bingo_boards.iter_mut() {
-    //         for single_board_row in single_board.iter_mut() {
-    //             for single_input in single_board_row.iter_mut() {
-    //                 if check_bingo(*single_board) {
-    //                     println!("{:?}", single_board);
-    //                     break;
-    //                 } else {
-    //                     if single_input.0 == num_to_mark {
-    //                         single_input.1 = true;
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
-
-    // let mut bingo_boards_clone = bingo_boards.clone();
-
-    // let result = bingo_input.iter().map(|num_to_mark| {
-    //     bingo_boards_clone.iter_mut().map(|single_board| {
-    //         let marked_num = mark_number(single_board, num_to_mark);
-    //         if marked_num {
-    //             if check_bingo(&single_board) {
-    //                 sum_of_all_unmarked_numbers(&single_board) * num_to_mark.clone();
-    //             }
-    //         }
-    //     })
-    // });
-
-    let _result = do_thing(bingo_input, bingo_boards);
-
-    // println!("Result: {:?}", result);
-
-    Ok(())
+    let mut result = 0;
+    for i in bingo_input {
+        let mut condition = false;
+        for j in 0..bingo_board_s.len() {
+            mark_board(i, &mut bingo_board_s[j]);
+            if check_bingo(&bingo_board_s[j]) {
+                println!("Bingo: {:?}", bingo_board_s[j]);
+                let temp = add_umarked(&bingo_board_s[j]);
+                println!("I: {}, temp: {}", i, temp);
+                result = i * temp;
+                condition = true;
+                break;
+            }
+        }
+        if condition {
+            break;
+        }
+    }
+    println!("Result: {}", result);
+    // println!("Bingo Input: {:?}", bingo_input);
+    // println!("Bingo Boards: {:?}", bingo_board_s);
+}
+fn add_umarked(input: &Vec<Vec<(usize, bool)>>) -> usize {
+    let mut output = 0;
+    for i in 0..5 {
+        for j in 0..5 {
+            if input[i][j].1 == false {
+                output = output + input[i][j].0;
+            }
+        }
+    }
+    output
+}
+fn check_bingo(bingo_board: &Vec<Vec<(usize, bool)>>) -> bool {
+    // row
+    // println!("In check_bingo");
+    for i in 0..5 {
+        for j in 0..5 {
+            if bingo_board[i][j].1 == false {
+                break;
+            } else if bingo_board[i][j].1 == true && j == 4 {
+                return true;
+            }
+        }
+    }
+    // column
+    for i in 0..5 {
+        for j in 0..5 {
+            if bingo_board[j][i].1 == false {
+                break;
+            }  else if bingo_board[j][i].1 == true && j == 4 {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+fn mark_board(bingo_input: usize, bingo_board: &mut Vec<Vec<(usize, bool)>>) {
+    for l in 0..5 {
+        for m in 0..5 {
+            if bingo_board[l][m].0 == bingo_input {
+                bingo_board[l][m].1 = true
+            }
+        }
+    }
 }
 
-fn make_bingo_board_and_input(
-    input: &mut std::str::Lines<'_>,
-) -> (Vec<usize>, Vec<Vec<Vec<(usize, bool)>>>) {
-    let mut bingo_boards: Vec<Vec<Vec<(usize, bool)>>> = Vec::new();
-
-    let bingo_input: Vec<usize> = input
+fn parse_input(input: &mut std::str::Lines<'_>) -> (Vec<usize>, Vec<Vec<Vec<(usize, bool)>>>) {
+    let output_one: Vec<usize> = input
         .next()
         .unwrap()
         .trim()
         .split(",")
         .map(|x| x.parse::<usize>().unwrap())
         .collect();
-
-    // while let line = input.next() {
-    //     println!("{:?}", line);
-    // }
-
-    // for line in input.next() {
-    //   println!("line :{}", line);
-    // } println!("line :{}", line);
-
-    let mut single_board: Vec<Vec<(usize, bool)>> = Vec::new();
+    let mut output_two: Vec<Vec<Vec<(usize, bool)>>> = Vec::new();
+    let mut temp_as_arrays: Vec<Vec<(usize, bool)>> = Vec::new();
+    let mut iter = 0;
     loop {
-        let get_line = input.next();
-
-        // println!("{:?}", get_line);
-
-        match get_line {
-            Some(single_line) => {
-                if single_line.is_empty() {
-                    continue;
-                } else {
-                    if single_board.len() < 4 {
-                        // println!("single board: {:?}", single_board);
-                        let get_single_board_one_line = single_line
-                            .trim()
-                            .split_ascii_whitespace()
-                            .map(|i| (i.parse::<usize>().unwrap(), false))
-                            .collect::<Vec<(usize, bool)>>();
-                        single_board.push(get_single_board_one_line);
-                    } else if single_board.len() == 4 {
-                        let get_single_board_one_line = single_line
-                            .trim()
-                            .split_ascii_whitespace()
-                            .map(|i| (i.parse::<usize>().unwrap(), false))
-                            .collect::<Vec<(usize, bool)>>();
-                        single_board.push(get_single_board_one_line);
-                        bingo_boards.push(single_board);
-                        single_board = Vec::new();
-                    } else {
-                        println!("Something strange happend 1");
-                    }
+        match input.next() {
+            Some(x) => {
+                if x != "" {
+                    let temp_array = x
+                        .trim()
+                        .split_ascii_whitespace()
+                        .map(|f| (f.parse::<usize>().unwrap(), false))
+                        .collect();
+                    // println!("Temp Array: {:?}", temp_array);
+                    temp_as_arrays.push(temp_array);
+                    iter = iter + 1;
+                    // println!("Iter: {}", iter);
+                    // println!("Temp board: {:?}", temp_board);
                 }
             }
             None => break,
         }
     }
+    let mut temp: Vec<Vec<(usize, bool)>> = Vec::new();
+    let mut i = 0;
 
-    // println!("{:?}", input);
-
-    (bingo_input, bingo_boards)
-}
-
-fn check_bingo(one_board: &Vec<Vec<(usize, bool)>>) -> bool {
-    // horizontal
-    // println!("Cehck Bingo");
-    let (mut loop_no_row, mut loop_no_column, mut main_loop) = (0, 0, 0);
-    for i in 0..5 {
-        for j in 0..5 {
-            if one_board[i][j].1 == false {
-                break;
-            } else if one_board[i][j].1 == true && j == 4 {
-                println!("Wining board Row: {:?}", one_board);
-                loop_no_row = loop_no_row + 1;
-                return true;
-            }
-        }
-    }
-    // vertical
-    for i in 0..5 {
-        for j in 0..5 {
-            if one_board[j][i].1 == false {
-                break;
-            } else if one_board[j][i].1 == true && j == 4 {
-                println!("Wining board Column: {:?}", one_board);
-                loop_no_column = loop_no_column + 1;
-                return true;
-            }
-        }
-    }
-    main_loop = main_loop + 1;
-    println!(
-        "mainLoop:{}, rowLoop:{}, colLoop:{}",
-        main_loop, loop_no_row, loop_no_column
-    );
-    return false;
-}
-
-fn mark_number(single_board: &mut Vec<Vec<(usize, bool)>>, num_to_mark: usize) -> bool {
-    // let inner_num_to_mark = num_to_mark.clone();
-    for i in 0..5 {
-        for j in 0..5 {
-            if single_board[i][j].0 == num_to_mark {
-                // println!("Before: {}", single_board[i][j].1);
-                single_board[i][j].1 = true;
-                // println!("bool: {}", single_board[i][j].1);
-                // println!("value: {}", single_board[i][j].0);
-                // println!("After: {:?}", single_board);
-                return true;
-            }
-        }
-    }
-    // println!("Mark Number {:?}", single_board);
-    return false;
-}
-fn sum_of_all_unmarked_numbers(single_board: &Vec<Vec<(usize, bool)>>) -> usize {
-    let mut result = 0;
-    for i in 0..5 {
-        for j in 0..5 {
-            if single_board[i][j].1 == false {
-                result = result + single_board[i][j].0;
-            }
-        }
-    }
-    result
-}
-
-fn do_thing(bingo_input: Vec<usize>, bingo_boards: Vec<Vec<Vec<(usize, bool)>>>) -> usize {
-    // let mut no_loop_one = 0;
-    // let mut no_loop_two = 0;
-
-    for num_to_mark in bingo_input.iter() {
-        for single_board in bingo_boards.clone().iter_mut() {
-            let marked_num = mark_number(single_board, *num_to_mark);
-            // print!("In main loop single board{:?}", single_board);
-            if marked_num {
-                // println!("Here");
-                if check_bingo(single_board) {
-                    println!("Is this the problem");
-                    return sum_of_all_unmarked_numbers(single_board) * num_to_mark;
-                    // println!("Result in loop:{}", result);
+    loop {
+        if i < temp_as_arrays.len() {
+            if temp.len() < 5 {
+                temp.push(temp_as_arrays[i].clone());
+                i = i + 1;
+                if i == temp_as_arrays.len() {
+                    output_two.push(temp);
+                    break;
                 }
+                // println!("I1: {}", i)
+            } else if temp.len() == 5 {
+                // println!("I2: {}", i);
+                output_two.push(temp);
+                temp = Vec::new();
             }
-            // no_loop_two = no_loop_two + 1;
-            // println!("Two: {}", no_loop_two);
+        } else {
+            break;
         }
-        // no_loop_one = no_loop_one + 1;
-        // println!("One: {}", no_loop_one);
     }
-    panic!("It doesn't work without it");
+    // println!("Output two: {:?}", output_two);
+    (output_one, output_two)
 }
